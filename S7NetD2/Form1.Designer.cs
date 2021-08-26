c10726005b8bca73bb999385feb28a500f0e8238
furkanaygul <58143573+furkanaygul@users.noreply.github.com>
2021-08-26 14:50:51 +0300

    PLC-Profinet With C#

---
+++ S7NetD2/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace S7NetD2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPBaglanti = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnMotorClose = new System.Windows.Forms.Button();
+            this.btnMotorRun = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CbCpu = new System.Windows.Forms.ComboBox();
+            this.txtSlot = new System.Windows.Forms.TextBox();
+            this.txtRack = new System.Windows.Forms.TextBox();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ReadWrite = new System.Windows.Forms.TabPage();
+            this.saat = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPBaglanti.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPBaglanti);
+            this.tabControl1.Controls.Add(this.ReadWrite);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPBaglanti
+            // 
+            this.tabPBaglanti.Controls.Add(this.label5);
+            this.tabPBaglanti.Controls.Add(this.groupBox2);
+            this.tabPBaglanti.Controls.Add(this.btnDisconnect);
+            this.tabPBaglanti.Controls.Add(this.btnConnect);
+            this.tabPBaglanti.Controls.Add(this.groupBox1);
+            this.tabPBaglanti.Location = new System.Drawing.Point(4, 25);
+            this.tabPBaglanti.Name = "tabPBaglanti";
+            this.tabPBaglanti.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPBaglanti.Size = new System.Drawing.Size(768, 397);
+            this.tabPBaglanti.TabIndex = 0;
+            this.tabPBaglanti.Text = "Bağlantı";
+            this.tabPBaglanti.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(376, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 29);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.btnMotorClose);
+            this.groupBox2.Controls.Add(this.btnMotorRun);
+            this.groupBox2.Location = new System.Drawing.Point(376, 60);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(358, 207);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Motor";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::S7NetD2.Properties.Resources.Untitled;
+            this.pictureBox1.Location = new System.Drawing.Point(68, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnMotorClose
+            // 
+            this.btnMotorClose.Location = new System.Drawing.Point(180, 157);
+            this.btnMotorClose.Name = "btnMotorClose";
+            this.btnMotorClose.Size = new System.Drawing.Size(111, 41);
+            this.btnMotorClose.TabIndex = 1;
+            this.btnMotorClose.Text = "Kapat";
+            this.btnMotorClose.UseVisualStyleBackColor = true;
+            this.btnMotorClose.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnMotorRun
+            // 
+            this.btnMotorRun.Location = new System.Drawing.Point(47, 157);
+            this.btnMotorRun.Name = "btnMotorRun";
+            this.btnMotorRun.Size = new System.Drawing.Size(111, 41);
+            this.btnMotorRun.TabIndex = 0;
+            this.btnMotorRun.Text = "Çalıştır";
+            this.btnMotorRun.UseVisualStyleBackColor = true;
+            this.btnMotorRun.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(229, 221);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(141, 46);
+            this.btnDisconnect.TabIndex = 3;
+            this.btnDisconnect.Text = "Bağlantıyı Kes";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(82, 221);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(141, 46);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Bağlan";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CbCpu);
+            this.groupBox1.Controls.Add(this.txtSlot);
+            this.groupBox1.Controls.Add(this.txtRack);
+            this.groupBox1.Controls.Add(this.txtIp);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(364, 209);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bağlantı";
+            // 
+            // CbCpu
+            // 
+            this.CbCpu.FormattingEnabled = true;
+            this.CbCpu.Location = new System.Drawing.Point(133, 38);
+            this.CbCpu.Name = "CbCpu";
+            this.CbCpu.Size = new System.Drawing.Size(186, 24);
+            this.CbCpu.TabIndex = 9;
+            // 
+            // txtSlot
+            // 
+            this.txtSlot.Location = new System.Drawing.Point(133, 152);
+            this.txtSlot.Name = "txtSlot";
+            this.txtSlot.Size = new System.Drawing.Size(186, 22);
+            this.txtSlot.TabIndex = 8;
+            this.txtSlot.Text = "1";
+            // 
+            // txtRack
+            // 
+            this.txtRack.Location = new System.Drawing.Point(133, 113);
+            this.txtRack.Name = "txtRack";
+            this.txtRack.Size = new System.Drawing.Size(186, 22);
+            this.txtRack.TabIndex = 7;
+            this.txtRack.Text = "0";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(133, 78);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(186, 22);
+            this.txtIp.TabIndex = 6;
+            this.txtIp.Text = "192.168.0.2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Slot";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Rack";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "IPadresi";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PLC";
+            // 
+            // ReadWrite
+            // 
+            this.ReadWrite.Location = new System.Drawing.Point(4, 25);
+            this.ReadWrite.Name = "ReadWrite";
+            this.ReadWrite.Padding = new System.Windows.Forms.Padding(3);
+            this.ReadWrite.Size = new System.Drawing.Size(768, 397);
+            this.ReadWrite.TabIndex = 1;
+            this.ReadWrite.Text = "Oku-Yaz";
+            this.ReadWrite.UseVisualStyleBackColor = true;
+            // 
+            // saat
+            // 
+            this.saat.Enabled = true;
+            this.saat.Interval = 1000;
+            this.saat.Tick += new System.EventHandler(this.saat_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 444);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPBaglanti.ResumeLayout(false);
+            this.tabPBaglanti.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPBaglanti;
+        private System.Windows.Forms.TabPage ReadWrite;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox CbCpu;
+        private System.Windows.Forms.TextBox txtSlot;
+        private System.Windows.Forms.TextBox txtRack;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnMotorClose;
+        private System.Windows.Forms.Button btnMotorRun;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer saat;
+    }
+}
+
---
+++ S7NetD2/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using S7.Net;
+namespace S7NetD2
+{
+    public partial class Form1 : Form
+    {
+        private Plc plc = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            CbCpu.DataSource = Enum.GetNames(typeof(CpuType));// comboboxa cpu tiplerini atadik
+            CbCpu.SelectedIndex = 5;//default s7-1200 secildi
+            btnDisconnect.Enabled = false;
+            btnMotorRun.Enabled = false;
+            groupBox2.Enabled = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (plc != null)
+            {
+                plc.Close();
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            CpuType cpuType = (CpuType)Enum.Parse(typeof(CpuType), CbCpu.Text);
+            string adress = txtIp.Text;
+            short rack = short.Parse(txtRack.Text);
+            short slot = short.Parse(txtSlot.Text);
+            //plc = new Plc(cpuType, adress, rack, slot);
+            plc = new Plc(cpuType, adress, rack, slot);
+            try
+            {
+                plc.Open();
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+                groupBox1.Enabled = false;
+                btnMotorRun.Enabled = true;
+
+                groupBox2.Enabled = true;
+                //BOOL
+                plc.Write("DB1.DBX0.0", false);
+                //BOOL
+                plc.Write("DB1.DBX0.1", true);
+                //INT 16 bit integer
+                plc.Write("DB1.DBW2.0", (Int16)536);
+                //Real degeri 
+                plc.Write("DB1.DBD4.0", (float)32.14);
+                //Dint yani 32bit normal int
+                plc.Write("DB1.DBD8.0", -134);
+                //WORD
+                plc.Write("DB1.DBD12.0", (uint)67);
+                //WORD
+                plc.Write("DB1.DBW16.0", (ushort)87);
+
+
+
+
+                bool db1Bool1 = (bool)plc.Read("DB1.DBX0.0");
+                Console.WriteLine("DB1.DBX0.0 " + db1Bool1);
+                bool db1Bool2 = (bool)plc.Read("DB1.DBX0.1");
+                Console.WriteLine("DB1.DBX0.1 " + db1Bool2);
+                var db1INT = (ushort)plc.Read("DB1.DBW2.0");
+                Console.WriteLine("DB1.DBW2.0 " + db1INT);
+                var db1REAL = ((uint)plc.Read("DB1.DBD4.0")).ConvertToFloat();
+                Console.WriteLine("DB1.DBD4.0 " + db1REAL);
+                var db1DINT = ((uint)plc.Read("DB1.DBD8.0")).ConvertToInt();
+                Console.WriteLine("DB1.DBD8.0 " + db1DINT);
+                var db1DWord = ((uint)plc.Read("DB1.DBD12.0")).ToString("X8");
+                Console.WriteLine("DB1.DBD12.0 " + db1DWord);
+                var db1Word = ((ushort)plc.Read("DB1.DBW16.0")).ToString("X4");
+                Console.WriteLine("DB1.DBD16.0 " + db1Word);
+                var db1Time = (uint)plc.Read("DB1.DBD18.0");
+                Console.WriteLine("DB1.DBD18.0" + db1Time);
+
+                ////NEGATIF
+                //plc.Write("DB2.DBX0.0", true);//bool
+                //plc.Write("DB2.DBX0.1", false);//bool
+                //short value = -340;
+                //plc.Write("DB2.DBW2.0", value.ConvertToUshort());//int
+                //float valuee = 3.1245f;
+                //plc.Write("DB2.DBD4.0", valuee);//real
+                //ushort val = 10000;
+                //plc.Write("DB2.DBW16", val);
+
+
+                //bool db2Bool1 = (bool)plc.Read("DB2.DBX0.0");
+                //Console.WriteLine("DB2.DBX0.0 " + db1Bool1);
+                //bool db2Bool2 = (bool)plc.Read("DB2.DBX0.1");
+                //Console.WriteLine("DB2.DBX0.1 " + db1Bool2);
+                //short db2INT = ((ushort)plc.Read("DB2.DBW2")).ConvertToShort();
+                //Console.WriteLine("DB2.DBW2.0 " + db1INT);
+                //var db2REAL = ((uint)plc.Read("DB2.DBD4.0")).ConvertToFloat();
+                //Console.WriteLine("DB2.DBD4.0 " + db1REAL);
+                //var db2DINT = (uint)plc.Read("DB2.DBD8.0");
+                //Console.WriteLine("DB2.DBD8.0 " + db1DINT);
+                //var db2DWord = ((uint)plc.Read("DB2.DBD12.0"));
+                //Console.WriteLine("DB2.DBD12.0 " + db1DWord);
+                //ushort db2Word = ((ushort)plc.Read("DB2.DBW16"));
+                //Console.WriteLine("DB2.DBD16.0 " + db1Word);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(this, "PLC ile baglanti kurulamadi", "Uyari", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (plc.IsConnected)
+                {
+                    plc.Write("DB1.DBX24.0", true);
+                    plc.Write("DB1.DBX24.1", false);
+                    if ((bool)plc.Read("Q0.0"))
+                    {
+                        btnMotorRun.Enabled = false;
+                        btnMotorClose.Enabled = true;
+                        pictureBox1.Image = Properties.Resources.engine;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(this, ex.Message, "Uyari", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (plc.IsConnected)
+                {
+                    plc.Close();
+                    btnDisconnect.Enabled = false;
+                    btnConnect.Enabled = true;
+                    groupBox1.Enabled = true;
+                    groupBox2.Enabled = false;
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(this, ex.Message, "Uyari", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (plc.IsConnected)
+                {
+                    plc.Write("DB1.DBX24.0", false);
+                    plc.Write("DB1.DBX24.1", true);
+                    btnMotorRun.Enabled = true;
+                    btnMotorClose.Enabled = false;
+                    pictureBox1.Image = Properties.Resources.Untitled;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(this, ex.Message, "Uyari", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void saat_Tick(object sender, EventArgs e)
+        {
+            label5.Text = DateTime.Now.ToString();
+        }
+    }
+}
